6de727e909650922ad4e51bfe57834780ce204f4
aryyya <aryyya.xyz@gmail.com>
2019-05-18 11:39:33 -0700

    Add router for auth endpoints.

--- src/main.js
+++ src/main.js
@@ -2,19 +2,31 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
+// server init
+
 const server = express()
+
+// middleware
 
 server.use(morgan('dev'))
 
 server.use(bodyParser.json())
 
+// root routes
+
 server.get('/', (req, res) => {
   res.end('Hello, world!')
 })
 
+// other routes
+
+server.use('/auth', require('./routes/auth'))
+
 server.get('*', (req, res) => {
   res.sendStatus(404)
 })
+
+// start server
 
 const port = process.env.PORT || 8085
 server.listen(port, () => {
