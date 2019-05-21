8c3ca007b5b4f82a6af09afd79c675a38e30a8a0
aryyya <aryyya.xyz@gmail.com>
2019-05-20 21:14:46 -0700

    Move validation middleware and utility functions to generic module.

--- src/utility.js
+++ src/utility.js
@@ -1,5 +1,10 @@
 const jwt = require('jsonwebtoken')
 const expressjwt = require('express-jwt')
+const {
+  OK,
+  BAD_REQUEST
+} = require('http-status')
+const { validationResult } = require('express-validator/check')
 
 const secret = process.env.AUTH_SECRET || 'this is a secret'
 const expiresIn = '3 hours'
@@ -12,7 +17,48 @@ const hasAuthToken = expressjwt({
   secret
 })
 
+const checkValidationErrors = (request, response, next) => {
+  const errors = validationResult(request)
+  if (!errors.isEmpty()) {
+    return sendValidationErrorsResponse(response, errors)
+  }
+  next()
+}
+
+const sendValidationErrorsResponse = (response, errors) => {
+  response
+    .status(BAD_REQUEST)
+    .json({
+      errors: errors.array()
+    })
+}
+
+const sendInvalidCredentialsResponse = response => {
+  response
+    .status(BAD_REQUEST)
+    .json({
+      errors: [
+        'invalid credentials'
+      ]
+    })
+}
+
+const sendAuthTokenResponse = (response, user) => {
+  response
+    .status(OK)
+    .send({
+      authToken: getAuthToken({
+        id: user.id,
+        username: user.username
+      })
+    })
+}
+
 module.exports = {
   getAuthToken,
-  hasAuthToken
+  hasAuthToken,
+  checkValidationErrors,
+  sendValidationErrorsResponse,
+  sendInvalidCredentialsResponse,
+  sendAuthTokenResponse
 }
