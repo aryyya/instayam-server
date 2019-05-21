e4e75f6a3d5090450b5104b7b8c7ac877e2bc482
aryyya <aryyya.xyz@gmail.com>
2019-05-20 17:27:07 -0700

    Add sign up route.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,16 @@
 const { Router } = require('express')
 const { getAuthToken } = require('../utility')
-const { compare } = require('bcrypt')
+const {
+  compare,
+  hash
+} = require('bcrypt')
 const { User } = require('../models')
 const {
   OK,
   BAD_REQUEST,
   UNAUTHORIZED
 } = require('http-status')
+const { ValidationError } = require('sequelize')
 
 const router = Router()
 
@@ -53,15 +57,76 @@ router.post('/login', async (request, response) => {
       response
         .status(BAD_REQUEST)
         .send('required parameters: username, password')
-      return
     }
 
-    if (error === 'INVALID_CREDENTIALS' || error === 'USER_NOT_FOUND') {
+    else if (error === 'INVALID_CREDENTIALS' || error === 'USER_NOT_FOUND') {
       response
         .status(UNAUTHORIZED)
         .send('invalid credentials')
-      return
     }
+
+    else throw error
+  }
+})
+
+router.post('/sign-up', async (request, response) => {
+  try {
+    const {
+      email,
+      username,
+      password,
+      fullName
+    } = request.body
+
+    if (!email || !username || !password || !fullName) {
+      throw 'MISSING_PARAMETERS'
+    }
+
+    if (password.length < 8 || password.length > 64) {
+      throw 'INVALID_PASSWORD_LENGTH'
+    }
+
+    const password_hash = await hash(password, 10)
+
+    const user = await User.create({
+      email,
+      username,
+      password_hash,
+      full_name: fullName
+    })
+
+    const authToken = getAuthToken({
+      id: user.id,
+      username: user.username
+    })
+
+    response
+      .status(OK)
+      .send({
+        authToken
+      })
+    return
+  }
+  catch (error) {
+    if (error === 'MISSING_PARAMETERS') {
+      response
+        .status(BAD_REQUEST)
+        .send('required parameters: email, username, password, fullName')
+    }
+
+    else if (error === 'INVALID_PASSWORD_LENGTH') {
+      response
+        .status(BAD_REQUEST)
+        .send('password length must be between 8 and 64 characters')
+    }
+
+    else if (error instanceof ValidationError) {
+      response
+        .status(BAD_REQUEST)
+        .send('there was a validation error')
+    }
+
+    else throw error
   }
 })
 
